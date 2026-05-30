cc9fdd98ba855743edb7b590fea7f7413d216da7
ackey1007fw-coder <ackey1007fw@gmail.com>
2026-05-31 06:00:47 +0900

    feat: add SHOWROOM 夢の国 event (6/4-6/10)

--- src/data/events.ts
+++ src/data/events.ts
@@ -5,26 +5,44 @@ const driveImage = (id: string) =>
 
 export const events: ScheduleEvent[] = [
   {
+    id: "showroom-dreamisland-2026-06-04",
+    title: "SHOWROOM「夢の国に遊びに行こう！テーマパークペアチケット＆駅中広告ポスター掲載権獲得イベント！」",
+    shortTitle: "夢の国イベント",
+    category: "event",
+    startAt: "2026-06-04T18:00:00+09:00",
+    endAt: "2026-06-10T21:59:00+09:00",
+    displayDate: "2026年6月4日（木）18:00〜6月10日（水）21:59",
+    image: driveImage("1hg7CCMX-vtLQo1CajCz0rc8_FZCddz-W"),
+    summary: "SHOWROOMのイベントでランキング1位になると、夢のテーマパークへのペアチケットをプレゼント！お姉ちゃんと一緒に行けるよう、配信を見て全力で応援しよう。",
+    badges: ["SHOWROOM", "イベント", "6/4〜6/10", "ランキング"],
+    links: [
+      { label: "イベントページ", url: "https://www.showroom-live.com/event/dreamisland6", kind: "stream" },
+      { label: "SHOWROOMへ", url: "https://www.showroom-live.com/room/profile?room_id=550336", kind: "stream" }
+    ],
+    isImportant: true
+  },
+
+  {
     id: "yofukashi-campus-2026-05-09",
-    title: "みつきとななこの夜ふかしキャンパス vol.2 春の新学期わくわくSP",
-    shortTitle: "夜ふかしキャンパス",
+    title: "ã¿ã¤ãã¨ãªãªãã®å¤ãµããã­ã£ã³ãã¹ vol.2 æ¥ã®æ°å­¦æããããSP",
+    shortTitle: "å¤ãµããã­ã£ã³ãã¹",
     category: "radio",
     startAt: "2026-05-09T17:00:00+09:00",
     endAt: "2026-05-09T19:45:00+09:00",
-    displayDate: "2026年5月9日（土）17:00から",
-    venue: "WALLOP押上スタジオ",
+    displayDate: "2026å¹´5æ9æ¥ï¼åï¼17:00ãã",
+    venue: "WALLOPæ¼ä¸ã¹ã¿ã¸ãª",
     image: driveImage("1A8rODe-N__YMWSjNEL5hNeT0U5gaT7Ka"),
     summary:
-      "公開収録と特典会を予定。公開収録は17:00から18:00、特典会は18:15から19:45です。",
-    badges: ["NEXT", "ラジオ", "公開収録", "特典会"],
+      "å¬éåé²ã¨ç¹å¸ä¼ãäºå®ãå¬éåé²ã¯17:00ãã18:00ãç¹å¸ä¼ã¯18:15ãã19:45ã§ãã",
+    badges: ["NEXT", "ã©ã¸ãª", "å¬éåé²", "ç¹å¸ä¼"],
     links: [
       {
-        label: "tigetで予約",
+        label: "tigetã§äºç´",
         url: "https://tiget.net/events/481711",
         kind: "ticket"
       },
       {
-        label: "WALLOPページ",
+        label: "WALLOPãã¼ã¸",
         url: "https://www.wallop.tv/",
         kind: "info"
       }
@@ -33,30 +51,30 @@ export const events: ScheduleEvent[] = [
   },
   {
     id: "theoridea-2026-05-14",
-    title: "劇場公演『ギリシャ神話戦記テオリデア アトランティスの残響』",
-    shortTitle: "テオリデア",
+    title: "åå ´å¬æ¼ãã®ãªã·ã£ç¥è©±æ¦è¨ããªãªãã¢ ã¢ãã©ã³ãã£ã¹ã®æ®é¿ã",
+    shortTitle: "ããªãªãã¢",
     category: "stage",
     startAt: "2026-05-14T18:30:00+09:00",
     endAt: "2026-05-17T20:30:00+09:00",
-    displayDate: "2026年5月14日（木）から5月17日（日）",
-    venue: "萬劇場（大塚）",
+    displayDate: "2026å¹´5æ14æ¥ï¼æ¨ï¼ãã5æ17æ¥ï¼æ¥ï¼",
+    venue: "è¬åå ´ï¼å¤§å¡ï¼",
     image: driveImage("1AzWL5zxhUWd2l4_Clk6SfR-eNsjwuRmg"),
     summary:
-      "初挑戦の舞台出演。5月16日はSpecial Live公演、5月17日は千秋楽。現地チケットと配信チケットがあります。",
-    badges: ["舞台", "チケット発売中", "配信あり", "重要"],
+      "åææ¦ã®èå°åºæ¼ã5æ16æ¥ã¯Special Liveå¬æ¼ã5æ17æ¥ã¯åç§æ¥½ãç¾å°ãã±ããã¨éä¿¡ãã±ãããããã¾ãã",
+    badges: ["èå°", "ãã±ããçºå£²ä¸­", "éä¿¡ãã", "éè¦"],
     links: [
       {
-        label: "チケット予約",
+        label: "ãã±ããäºç´",
         url: "https://www.quartet-online.net/ticket/sankyou2026?m=03hggeh",
         kind: "ticket"
       },
       {
-        label: "配信チケット",
+        label: "éä¿¡ãã±ãã",
         url: "https://twitcasting.tv/",
         kind: "stream"
       },
       {
-        label: "会場アクセス",
+        label: "ä¼å ´ã¢ã¯ã»ã¹",
         url: "https://yorozu-s.com/",
         kind: "info"
       }
@@ -65,20 +83,20 @@ export const events: ScheduleEvent[] = [
   },
   {
     id: "aitoki-2026-05-29",
-    title: "月シア別冊第一集『I'm talking about lovin'』",
-    shortTitle: "月シア別冊",
+    title: "æã·ã¢å¥åç¬¬ä¸éãI'm talking about lovin'ã",
+    shortTitle: "æã·ã¢å¥å",
     category: "stage",
     startAt: "2026-05-29T19:00:00+09:00",
     endAt: "2026-06-02T21:00:00+09:00",
-    displayDate: "2026年5月29日（金）から6月2日（火）",
-    venue: "西荻窪 遊空間がざびぃ",
+    displayDate: "2026å¹´5æ29æ¥ï¼éï¼ãã6æ2æ¥ï¼ç«ï¼",
+    venue: "è¥¿è»çªª éç©ºéããã³ã",
     image: driveImage("1hPMNv9kiFKfBKgp7CLfjv7zJBtTOKCx_"),
     summary:
-      "音楽と演劇が合わさった公演。Asideとして出演予定。全8回公演、前売券は6,000円です。",
-    badges: ["舞台", "先行販売中", "全8回", "アイトキ"],
+      "é³æ¥½ã¨æ¼åãåããã£ãå¬æ¼ãAsideã¨ãã¦åºæ¼äºå®ãå¨8åå¬æ¼ãåå£²å¸ã¯6,000åã§ãã",
+    badges: ["èå°", "åè¡è²©å£²ä¸­", "å¨8å", "ã¢ã¤ãã­"],
     links: [
       {
-        label: "チケット購入",
+        label: "ãã±ããè³¼å¥",
         url: "https://livepocket.jp/t/aitoki",
         kind: "ticket"
       },
@@ -92,24 +110,24 @@ export const events: ScheduleEvent[] = [
   },
   {
     id: "birthday-2026-06-24",
-    title: "りりちゃんのお誕生日",
-    shortTitle: "誕生日",
+    title: "ããã¡ããã®ãèªçæ¥",
+    shortTitle: "èªçæ¥",
     category: "birthday",
     startAt: "2026-06-24T00:00:00+09:00",
     endAt: "2026-06-24T23:59:59+09:00",
-    displayDate: "2026年6月24日（水）",
+    displayDate: "2026å¹´6æ24æ¥ï¼æ°´ï¼",
     image: driveImage("1rUFypXYABEcyRUaxhie6o_jaNBTAWR16"),
     summary:
-      "SNSやSHOWROOMでお祝いの気持ちを届けたい日。カウントダウンと一緒に準備できます。",
-    badges: ["特別", "Birthday"],
+      "SNSãSHOWROOMã§ãç¥ãã®æ°æã¡ãå±ãããæ¥ãã«ã¦ã³ããã¦ã³ã¨ä¸ç·ã«æºåã§ãã¾ãã",
+    badges: ["ç¹å¥", "Birthday"],
     links: [
       {
-        label: "Xでお祝いを見る",
+        label: "Xã§ãç¥ããè¦ã",
         url: "https://x.com/frecam2025_0306",
         kind: "sns"
       },
       {
-        label: "SHOWROOMへ",
+        label: "SHOWROOMã¸",
         url: "https://www.showroom-live.com/room/profile?room_id=550336",
         kind: "stream"
       }
@@ -117,25 +135,25 @@ export const events: ScheduleEvent[] = [
   },
   {
     id: "fukurow-fm-2026-04-21",
-    title: "ふくろうFM『ステラ HAPPYStyle！ CatchTheHeart』公開収録＆チェキ会＆特典会",
-    shortTitle: "ふくろうFM公開収録",
+    title: "ãµãããFMãã¹ãã© HAPPYStyleï¼ CatchTheHeartãå¬éåé²ï¼ãã§ã­ä¼ï¼ç¹å¸ä¼",
+    shortTitle: "ãµãããFMå¬éåé²",
     category: "radio",
     startAt: "2026-04-21T18:00:00+09:00",
     endAt: "2026-04-21T21:00:00+09:00",
-    displayDate: "2026年4月21日（火）",
-    venue: "曙橋スタジオ",
+    displayDate: "2026å¹´4æ21æ¥ï¼ç«ï¼",
+    venue: "ææ©ã¹ã¿ã¸ãª",
     image: driveImage("1uaNFIH1HkefOjn06tXwSJZvq8eIGjLBA"),
     summary:
-      "公開収録、チェキ会、特典会に出演。アーカイブがある場合は遠方からもチェックできます。",
-    badges: ["ラジオ", "公開収録", "特典会", "アーカイブ"],
+      "å¬éåé²ããã§ã­ä¼ãç¹å¸ä¼ã«åºæ¼ãã¢ã¼ã«ã¤ããããå ´åã¯é æ¹ããããã§ãã¯ã§ãã¾ãã",
+    badges: ["ã©ã¸ãª", "å¬éåé²", "ç¹å¸ä¼", "ã¢ã¼ã«ã¤ã"],
     links: [
       {
-        label: "ふくろうFMページ",
+        label: "ãµãããFMãã¼ã¸",
         url: "https://fukurowfm.co.jp/",
         kind: "info"
       },
       {
-        label: "Xで詳細",
+        label: "Xã§è©³ç´°",
         url: "https://x.com/frecam2025_0306",
         kind: "sns"
       }
@@ -143,19 +161,19 @@ export const events: ScheduleEvent[] = [
   },
   {
     id: "tvk-nekohita-2026-04-16",
-    title: "テレビ神奈川『猫のひたいほどワイドNEO』お天気コーナー",
-    shortTitle: "tvk猫ひた",
+    title: "ãã¬ãç¥å¥å·ãç«ã®ã²ããã»ã©ã¯ã¤ãNEOããå¤©æ°ã³ã¼ãã¼",
+    shortTitle: "tvkç«ã²ã",
     category: "tv",
     startAt: "2026-04-16T12:00:00+09:00",
     endAt: "2026-04-16T13:30:00+09:00",
-    displayDate: "2026年4月16日（木）12:00から13:30",
+    displayDate: "2026å¹´4æ16æ¥ï¼æ¨ï¼12:00ãã13:30",
     image: driveImage("1R8gptDr0DxZ71Ncuw8koDmgKo-lDjoYi"),
     summary:
-      "SHOWROOMイベントの特典として生放送テレビ出演。お天気コーナーに登場しました。",
-    badges: ["テレビ", "生放送", "お天気"],
+      "SHOWROOMã¤ãã³ãã®ç¹å¸ã¨ãã¦çæ¾éãã¬ãåºæ¼ããå¤©æ°ã³ã¼ãã¼ã«ç»å ´ãã¾ããã",
+    badges: ["ãã¬ã", "çæ¾é", "ãå¤©æ°"],
     links: [
       {
-        label: "tvkページ",
+        label: "tvkãã¼ã¸",
         url: "https://www.tvk-yokohama.com/",
         kind: "info"
       }
@@ -163,19 +181,19 @@ export const events: ScheduleEvent[] = [
   },
   {
     id: "imacampus-2026-04-12",
-    title: "MBSラジオ『イマドキッ キャンパスナイト』",
-    shortTitle: "イマキャン",
+    title: "MBSã©ã¸ãªãã¤ããã­ã ã­ã£ã³ãã¹ãã¤ãã",
+    shortTitle: "ã¤ãã­ã£ã³",
     category: "radio",
     startAt: "2026-04-12T23:30:00+09:00",
     endAt: "2026-04-13T00:00:00+09:00",
-    displayDate: "2026年4月12日（日）23:30から24:00",
+    displayDate: "2026å¹´4æ12æ¥ï¼æ¥ï¼23:30ãã24:00",
     image: driveImage("1FM8cYikXk6Z2TtAoHCpBkq_XOT_AmqbS"),
     summary:
-      "青学周辺のおすすめグルメなどを語ったラジオ出演回。声の魅力を楽しめる放送です。",
-    badges: ["ラジオ", "放送済み", "radiko"],
+      "éå­¦å¨è¾ºã®ããããã°ã«ã¡ãªã©ãèªã£ãã©ã¸ãªåºæ¼åãå£°ã®é­åãæ¥½ãããæ¾éã§ãã",
+    badges: ["ã©ã¸ãª", "æ¾éæ¸ã¿", "radiko"],
     links: [
       {
-        label: "radikoで探す",
+        label: "radikoã§æ¢ã",
         url: "https://radiko.jp/",
         kind: "stream"
       }
@@ -183,19 +201,19 @@ export const events: ScheduleEvent[] = [
   },
   {
     id: "steenz-2026-04-08",
-    title: "steenz 取材記事公開『フレキャン』",
-    shortTitle: "steenz記事公開",
+    title: "steenz åæè¨äºå¬éããã¬ã­ã£ã³ã",
+    shortTitle: "steenzè¨äºå¬é",
     category: "web",
     startAt: "2026-04-08T10:00:00+09:00",
     endAt: "2026-04-08T23:59:59+09:00",
-    displayDate: "2026年4月8日（水）",
+    displayDate: "2026å¹´4æ8æ¥ï¼æ°´ï¼",
     image: driveImage("1Amj4Ddj1QaQpv2IXO9qw2vHwS0wMXwGv"),
     summary:
-      "コンテストでの受賞や、青山学院大学生としての素顔、役者を目指す想いが読めるインタビュー記事です。",
-    badges: ["WEB", "インタビュー", "公開中"],
+      "ã³ã³ãã¹ãã§ã®åè³ããéå±±å­¦é¢å¤§å­¦çã¨ãã¦ã®ç´ é¡ãå½¹èãç®æãæ³ããèª­ããã¤ã³ã¿ãã¥ã¼è¨äºã§ãã",
+    badges: ["WEB", "ã¤ã³ã¿ãã¥ã¼", "å¬éä¸­"],
     links: [
       {
-        label: "steenzで読む",
+        label: "steenzã§èª­ã",
         url: "https://steenz.jp/",
         kind: "info"
       }
@@ -203,17 +221,17 @@ export const events: ScheduleEvent[] = [
   },
   {
     id: "kyanly-2026-03-14",
-    title: "KYANLYファン感謝祭2026 運動会＆チェキ会",
-    shortTitle: "KYANLY感謝祭",
+    title: "KYANLYãã¡ã³æè¬ç¥­2026 éåä¼ï¼ãã§ã­ä¼",
+    shortTitle: "KYANLYæè¬ç¥­",
     category: "event",
     startAt: "2026-03-14T11:00:00+09:00",
     endAt: "2026-03-14T16:00:00+09:00",
-    displayDate: "2026年3月14日（土）11:00から16:00",
-    venue: "東京都立川市 屋外施設",
+    displayDate: "2026å¹´3æ14æ¥ï¼åï¼11:00ãã16:00",
+    venue: "æ±äº¬é½ç«å·å¸ å±å¤æ½è¨­",
     image: driveImage("1D9t__MTdyoBzSHyK9-gxTWetikuHYnZ7"),
     summary:
-      "スポーツデーとチェキ撮影会に出演。ランウェイ姿はKYANLYの動画でもチェックできます。",
-    badges: ["イベント", "運動会", "チェキ会"],
+      "ã¹ãã¼ããã¼ã¨ãã§ã­æ®å½±ä¼ã«åºæ¼ãã©ã³ã¦ã§ã¤å§¿ã¯KYANLYã®åç»ã§ããã§ãã¯ã§ãã¾ãã",
+    badges: ["ã¤ãã³ã", "éåä¼", "ãã§ã­ä¼"],
     links: [
       {
         label: "KYANLY YouTube",
